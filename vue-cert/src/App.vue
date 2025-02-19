b2dc87be324fab6999affc91c4480f412fcf600d
Arno <avanderheever@gmail.com>
2025-02-19 17:19:26 +0200

    Practice template syntax and watchers

--- vue-cert/src/App.vue
+++ vue-cert/src/App.vue
@@ -1,85 +1,64 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from 'vue'
+
+//Template syntax
+const mainTitle = 'Vue Cert Practice Space'
+const dynamicId = ref("dynamicId")
+
+//Watchers
+const question = ref('')
+const answer = ref('Questions usually contain a questions mark. ;)')
+const loading = ref(false)
+
+watch(question, async (newQuestion, oldQuestion) => {
+  if (newQuestion.includes('?')) {
+    loading.value = true
+    answer.value = 'Thinking...'
+    try {
+      const res = await fetch('https://yesno.wtf/api')
+      answer.value = (await res.json()).answer
+    } catch (error) {
+      answer.value = 'Error! Could not reach API: ' + error
+    } finally {
+      loading.value = false
+      console.log('Question changed:', oldQuestion, '->', newQuestion)
+    }
+  } else {
+    answer.value = 'Questions usually contain a questions mark. ;)'
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div :id="dynamicId" class="vue-cert-practice-space">
+    <h1>{{ mainTitle }}</h1>
+    <p>A very basic site to practice the fundementals of Vue3</p>
+    <div class="watchers">
+      <h2>Watchers</h2>
+      <p>
+        Ask a yes/no question:
+        <input type="text" v-model="question" :disable="loading"/>
+      </p>
+      <p>{{ answer }}</p>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.vue-cert-practice-space {
+  display: grid;
+  align-content: flex-start;
+  gap: 1rem;
+  font-family: "Inter", serif;
+  min-height: 100vh;
+  background-color: #333;
+  color: #fff;
+  padding: 2rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.watchers {
+  display: grid;
+  gap: 0.5rem;
 }
 </style>
